fc9f2da71c654b9dff05819ce9904f0e486d9f7b
Rafiqul Islam Shawn <vwaaala@gmail.com>
2024-02-06 19:19:58 +0600

    fixed email verification, profile routes. removed unnecessary image route

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\DataTables\UsersDataTable;
+use App\Http\Requests\UserRequest;
+
+class UserController extends Controller
+{
+    public function index(UsersDataTable $dataTable)
+    {
+        return $dataTable->render('user.index');
+    }
+
+    public function create()
+    {
+
+        return view('user.create');
+    }
+
+    public function store(UserRequest $request)
+    {
+        if ($request->hasFile('avatar')) {
+            $image = $request->file('avatar');
+
+            // Generate a unique filename using the original file extension
+            $image_name = rand() . '.' . $image->getClientOriginalExtension();
+
+            // Store the file in the private disk with the new filename
+            $imagePath = $image->move(public_path('/assets/images/avatar/'), $image_name);
+        }
+//        dd($imagePath);
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'role_id' => $request->input('role'),
+            'status' => $request->input('status'),
+            'avatar' => $imagePath ?? '/assets/images/avatar/avatar.jpg',
+        ]);
+
+        return redirect()->route('user.index');
+    }
+
+    public function view($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.view', compact('user'));
+    }
+
+    public function profile()
+    {
+        return view('user.profile');
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('user.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validate and update the user
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+            // Add other fields as needed
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully');
+    }
+
+    public function destroy($id)
+    {
+        User::findOrFail($id)->delete();
+        return redirect()->route('user.index')->with('success', 'User deleted successfully');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/locale/{locale}', function ($locale) {
+
+})->name('locale');
+// Auth middleware for authenticated users
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/roles', [App\Http\Controllers\RoleController::class, 'index'])->name('role');
+    Route::get('/profile', [App\Http\Controllers\UserController::class, 'profile'])->name('profile');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/queries', [App\Http\Controllers\QueryController::class, 'index'])->name('query');
+
+
+    Route::get('users', [App\Http\Controllers\UserController::class, 'index'])->name('user.index');
+    Route::get('users/{id}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('user.edit');
+    Route::put('users/{id}', [App\Http\Controllers\UserController::class, 'update'])->name('user.update');
+    Route::get('users/{id}/view', [App\Http\Controllers\UserController::class, 'view'])->name('user.view');
+    Route::delete('users/{id}', [App\Http\Controllers\UserController::class, 'destroy'])->name('user.destroy');
+
+});
+
