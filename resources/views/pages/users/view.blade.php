703a9de2bd7e4ff7ab4f8ea2c05eba8c69e96d30
Rafiqul Islam Shawn <vwaaala@gmail.com>
2024-02-17 05:41:28 +0600

    ui: change in users view

--- resources/views/pages/users/view.blade.php
+++ resources/views/pages/users/view.blade.php
@@ -1,20 +1,30 @@
 @extends('layouts.app')
+
 @section('content')
-    <!-- Profile Card -->
     <div class="card">
+        <div class="card-header">
+            <div class="d-flex justify-content-between align-items-center">
+                <span>My Profile</span>
+                <a href="{{ route('users.edit', $user->id) }}" class="btn btn-secondary">Edit Profile</a>
+            </div>
+        </div>
+
         <div class="card-body">
-            <!-- Profile Photo -->
-            <img src="{{ asset($user->avatar) }}" class="img-fluid rounded-circle mx-auto d-block" alt="Your Profile Photo" style="max-width: 150px;">
+            <div class="row">
+                <!-- Profile Photo -->
+                <div class="col-md-3">
+                    <img src="{{ asset($user->avatar) }}" class="img-fluid rounded" alt="Your Profile Photo">
+                </div>
 
-            <!-- Full Name -->
-            <h3 class="text-center mt-3">{{ $user->name }}</h3>
+                <!-- Name and Email -->
+                <div class="col-md-9">
+                    <!-- Full Name -->
+                    <h3>{{ $user->name }}</h3>
 
-            <!-- Email -->
-            <p class="text-center"><strong>Email:</strong> {{ $user->email }}</p>
-
-            <!-- Role Information -->
-            <p class="text-center"><strong>Role:</strong> {{ $user->name }}</p>
-            <a href="{{ route('users.edit', $user->id) }}" class="btn btn-warning">Edit profile</a>
+                    <!-- Email with Mail Icon -->
+                    <p><strong><i class="bi bi-envelope-check"></i></strong> {{ $user->email }}</p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
